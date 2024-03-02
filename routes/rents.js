ab2c8e7e17259185a0a8aa17779d83ec92894afe
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-02 16:30:29 +0700

    get Chart (Extra credit)

--- routes/rents.js
+++ routes/rents.js
@@ -8,7 +8,7 @@ const router = express.Router({mergeParams:true});
 const {protect,authorize}  =require('../middleware/auth');
 
 router.route('/').get(protect,getRents).post(protect,authorize('admin','user'),addRent);
-
+router.route('/chart').get(protect,getChart);
 router.route('/:id').get(protect,getRent).put(protect,authorize('admin','user'),updateRent)
                     .delete(protect,authorize('admin','user'),deleteRent);
 
