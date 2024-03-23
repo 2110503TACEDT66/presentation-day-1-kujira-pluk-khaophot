3dd8ab5b341e48167d32cb7df94a2bda29ed9916
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-23 22:01:52 +0700

    add more car details to car model

--- models/Car.js
+++ models/Car.js
@@ -22,6 +22,34 @@ const CarSchema = new mongoose.Schema({
         unique : true,
         trim : true,
         maxlength:[50,'Car\'s can not be more than 50 characters']
+    },
+    dayRate: {
+        type: Number,
+        require : [true, 'Please add a day rate']
+    },
+    picture: {
+        type: String,
+        required: false,
+    },
+    topSpeed:{
+        type: Number,
+        require: [true, 'Please add car\'s speed (mph)']
+    },
+    seats: {
+        type: Number,
+        required: [true, "Please add number of seats"],
+    },
+    color: {
+        type: String,
+        require: false
+    },
+    fuelType:{
+        type: String,
+        require: false
+    },
+    cargoCapacity: {
+        type: Number,
+        require: false
     }
 },{
     toJSON: {virtuals:true},
